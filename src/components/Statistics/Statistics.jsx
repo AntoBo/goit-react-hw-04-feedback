a708a889c1892656a78c679cbdada8e3ed6fa00c
AntoBo <anton.bondarenkooooo@gmail.com>
2022-05-03 15:27:09 +0300

    Update Statistics.jsx

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,10 +1,16 @@
 const Statistics = ({ good, neutral, bad }) => {
   return (
-    <ul>
-      <li>Good: {good}</li>
-      <li>Neutral: {neutral}</li>
-      <li>Bad: {bad}</li>
-    </ul>
+    <>
+      <ul>
+        <li>Good: {good}</li>
+        <li>Neutral: {neutral}</li>
+        <li>Bad: {bad}</li>
+      </ul>
+      <ul>
+        <li>Total: </li>
+        <li></li>
+      </ul>
+    </>
   );
 };
 
