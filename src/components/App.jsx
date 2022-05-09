7640a67ab9080592eddd8a44bd1bb68e7ce5bf44
AntoBo <anton.bondarenkooooo@gmail.com>
2022-05-09 15:42:04 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,7 +5,7 @@ import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
 import Notification from './Notification/Notification';
 import Section from './Section/Section';
 
-//refactor it on hooks
+//refactor it on hooks !
 
 export class App extends Component {
   state = { good: 0, neutral: 0, bad: 0 };
